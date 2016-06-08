8723991af95b714c71654278be4bdb0b661c1ee8
Phylogeny <molecularphylo@gmail.com>
2016-06-08 08:51:10 -0700

    Version changed to 1.9-1.2.0

--- src/main/java/com/phylogeny/kaolinitetest/reference/Reference.java
+++ src/main/java/com/phylogeny/kaolinitetest/reference/Reference.java
@@ -6,7 +6,7 @@ public class Reference
     public static final String GROUP_ID = "kaolinitetest";
     public static final String MOD_NAME = "Kaolinite Test";
     public static final String MOD_PATH = "com.phylogeny." + GROUP_ID;
-    public static final String VERSION = "1.9-1.1.0";
+    public static final String VERSION = "1.9-1.2.0";
     public static final String CLIENT_CLASSPATH = MOD_PATH + ".proxy.ProxyClient";
     public static final String COMMON_CLASSPATH = MOD_PATH + ".proxy.ProxyCommon";
 
